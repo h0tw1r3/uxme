2a115cf3790f4eec295952583c1a61a47278c748
Jeffrey Clark <dude@zaplabs.com>
2015-08-30 15:24:06 -0500

    [buildbot] option files and osx universal script

---
+++ .buildbot/macosx/universal.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [ ! -d build/osx_clang/bin/x32/Release ]; then
+	echo "No x32 Release build"
+	exit 1
+fi
+if [ ! -d build/osx_clang/bin/x64/Release ]; then
+	echo "No x64 Release build"
+	exit 1
+fi
+
+pushd build/osx_clang/bin/x32/Release
+FILES=$(find . -maxdepth 1 -type f -executable)
+
+if [ -d ../../universal ]; then
+	rm -rf ../../universal
+fi
+mkdir -p ../../universal/Release
+
+for FILE in $FILES
+do
+	FILE64=$FILE
+	if [ ! -f ../../x64/Release/$FILE ]; then
+		if [ ! -f ../../x64/Release/${FILE}64 ]; then
+			echo "Unable to locate 64bit binary: $FILE"
+			exit 1
+		else
+			FILE64=${FILE}64
+		fi
+	fi
+	x86_64-apple-darwin11-lipo -create $FILE ../../x64/Release/$FILE64 -output ../../universal/Release/$FILE
+	[ -e /usr/bin/upx ] && /usr/bin/upx ../../universal/Release/$FILE
+done
+
+popd
