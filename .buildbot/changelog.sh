5fe0a59035039ebc0fd854d001ac272fbdf84eb0
Jeffrey Clark <dude@zaplabs.com>
2015-09-14 10:12:22 -0500

    [buildbot] split post release commits by author

--- .buildbot/changelog.sh
+++ .buildbot/changelog.sh
@@ -4,20 +4,27 @@ VERSION=$1
 IFS=$'\r\n'
 
 echo -e "# Changelog $VERSION #\n"
-echo -e "## Post Release ##\n"
+echo -e "## Post release commits ##"
 
-git --no-pager log --date=short --simplify-merges --pretty=format:"* %s" ${VERSION}..HEAD
+RANGE="${VERSION}..HEAD"
+for author in $(git --no-pager log --simplify-merges --pretty=format:"%an" ${RANGE} | sort | uniq -c) ;
+do
+	name=$(echo $author | sed 's/^\ *\([0-9]*\)\ \(.*\)$/\2/')
+	count=$(echo $author | sed 's/^\ *\([0-9]*\)\ \(.*\)$/\1/')
+	echo -e "\n### $name ($count commits)\n"
+	git --no-pager log --author="$name" --date=short --simplify-merges --pretty=format:"* %s" ${RANGE} | grep -v "(nw)\| NW\| nw"
+done
 
 MAMEVERSION="mame$(echo $VERSION | sed 's/[^0-9]//g')"
 PREVMAMEVERSION=$(git describe --tag --abbrev=0 ${MAMEVERSION}^)
 RANGE="${PREVMAMEVERSION}..${MAMEVERSION}"
 
-echo $RANGE
+echo -e "\n## Commits"
 
 for author in $(git --no-pager log --simplify-merges --pretty=format:"%an" ${RANGE} | sort | uniq -c) ;
 do
 	name=$(echo $author | sed 's/^\ *\([0-9]*\)\ \(.*\)$/\2/')
 	count=$(echo $author | sed 's/^\ *\([0-9]*\)\ \(.*\)$/\1/')
-	echo -e "\n## $name ($count commits)\n"
+	echo -e "\n### $name ($count commits)\n"
 	git --no-pager log --author="$name" --date=short --simplify-merges --pretty=format:"* %s" ${RANGE} | grep -v "(nw)\| NW\| nw"
 done
