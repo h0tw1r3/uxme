4e30ec92d577ef179d3e75f591c3e2ddfc5fa1fa
Jeffrey Clark <dude@zaplabs.com>
2015-10-22 08:43:54 -0500

    Silence Qt pkg-config messages when building without QT on linux

--- scripts/src/osd/sdl_cfg.lua
+++ scripts/src/osd/sdl_cfg.lua
@@ -124,14 +124,16 @@ if _OPTIONS["targetos"]=="windows" then
 	configuration { }
 
 elseif _OPTIONS["targetos"]=="linux" then
-	if _OPTIONS["QT_HOME"]~=nil then
-		buildoptions {
-			"-I" .. backtick(_OPTIONS["QT_HOME"] .. "/bin/qmake -query QT_INSTALL_HEADERS"),
-		}
-	else
-		buildoptions {
-			backtick("pkg-config --cflags QtGui"),
-		}
+	if _OPTIONS["USE_QTDEBUG"]==1 then
+		if _OPTIONS["QT_HOME"]~=nil then
+			buildoptions {
+				"-I" .. backtick(_OPTIONS["QT_HOME"] .. "/bin/qmake -query QT_INSTALL_HEADERS"),
+			}
+		else
+			buildoptions {
+				backtick("pkg-config --cflags QtGui"),
+			}
+		end
 	end
 elseif _OPTIONS["targetos"]=="macosx" then
 	defines {
